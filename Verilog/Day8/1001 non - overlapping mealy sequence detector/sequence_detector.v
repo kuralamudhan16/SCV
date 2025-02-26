80f5491bc8454b8e242ebabbe22840fcdc9ca5cf
Kuralalamudhan <118452831+kuralamudhan16@users.noreply.github.com>
2025-02-26 20:17:30 +0530

    Create sequence_detector.v

---
+++ Verilog/Day8/1001 non - overlapping mealy sequence detector/sequence_detector.v
@@ -0,0 +1,32 @@
+module sequence_detector(
+  input clk,reset,x,
+  output z);
+  
+  parameter A = 4'h1;
+  parameter B = 4'h2;
+  parameter C = 4'h3;
+  parameter D = 4'h4;
+  
+  bit[3:0]state,next_state;
+  always@(posedge clk or negedge reset)begin
+    if(!reset)begin
+      state<=A;
+    end
+    else state<=next_state;
+  end
+  
+  always@(*)begin
+    case(state)
+     A:next_state=x?B:A;
+     B:next_state=x?B:C;
+     C:next_state=x?B:D;
+     D:next_state=x?A:A;
+     default:next_state = A;
+    endcase
+  end
+  assign z = (state == D)&&(x == 0);
+endmodule
+        
+    
+  
+  

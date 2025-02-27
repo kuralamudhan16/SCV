0bd2f485cdf7a2e12d0fda11c81d51cc7865fe63
Kuralalamudhan <118452831+kuralamudhan16@users.noreply.github.com>
2025-02-27 19:51:28 +0530

    Create sequence_detector_tb.v

---
+++ Verilog/day9/1001 non-Overlapping Moore Sequence Detector/sequence_detector_tb.v
@@ -0,0 +1,38 @@
+module sequence_detector_tb;
+  reg clk,reset,x;
+  wire z;
+  
+  sequence_detector uut(clk,reset,x,z);
+  initial clk = 0;
+  always #2 clk=~clk;
+  
+  initial begin
+    $dumpfile("sequence_detector_tb.vcd");
+    $dumpvars(0);
+    
+    x = 0;
+    #1 reset = 0;
+    #2 reset = 1;
+    
+    #3 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 0;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #10;
+    $finish;
+  end
+endmodule
+    
+    
+    

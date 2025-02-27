c478b52c40eae3f52e9ca57c9abf7e8d64a13a68
Kuralalamudhan <118452831+kuralamudhan16@users.noreply.github.com>
2025-02-27 19:45:19 +0530

    Create johnson_counter_tb.v

---
+++ Verilog/day9/4 Bit Johnson Counter/johnson_counter_tb.v
@@ -0,0 +1,25 @@
+module johnson_counter_tb;
+  parameter WIDTH = 4;
+
+  reg clk;
+  reg reset;
+  wire [WIDTH-1:0] out;
+
+  johnson_counter 	utt (.clk (clk),
+                .reset (reset),
+                .out (out));
+
+  always #2 clk = ~clk;
+
+  initial begin
+    {clk, reset} <= 0;
+    $dumpfile("ring_counter_tb.vcd");
+    $dumpvars(0);
+    repeat (2) @(posedge clk);
+    reset <= 1;
+    repeat (15) @(posedge clk);
+    $finish;
+  end
+  initial     $monitor ("clk=%0b  reset=%0b  out=%0b",clk,reset,out);
+
+endmodule
